d596cf5f6429d0657dff7ad7a402192261da1ad3
Koki Oyatsu <kaishuu0123@gmail.com>
2024-11-14 10:40:31 +0900

    Change formatToRecent (yyyy/MM/dd)

--- src/renderer/src/lib/formatDateTime.ts
+++ src/renderer/src/lib/formatDateTime.ts
@@ -16,12 +16,9 @@ export const formatToRecent = (dateTimeMilliSeconds: number | undefined) => {
   const dateTime = new Date(dateTimeMilliSeconds)
   const now = new Date()
   const isFormatToTime = isSameDay(now, dateTime)
-  const isFormatToMonthAndDay = isSameYear(now, dateTime)
 
   if (isFormatToTime) {
     return format(dateTime, 'HH:mm:ss')
-  } else if (isFormatToMonthAndDay) {
-    return format(dateTime, 'yy/MM/dd')
   } else {
     return format(dateTime, 'yyyy/MM/dd')
   }
