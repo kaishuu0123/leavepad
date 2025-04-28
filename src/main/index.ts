fbaf4311dcc7281edf0278acf32f4615653dd36b
Koki Oyatsu <kaishuu0123@gmail.com>
2025-04-29 00:44:22 +0900

    update npm packages.
    - add mainWindow.webContents.reloadIgnoringCache() when is.dev

--- src/main/index.ts
+++ src/main/index.ts
@@ -57,6 +57,7 @@ function createWindow(): void {
   // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
+    mainWindow.webContents.reloadIgnoringCache()
   } else {
     mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
   }
